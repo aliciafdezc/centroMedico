5aa486350afc94d414e3b2697d65f412777c053b
aliciafdezc <cfdezalicia@gmail.com>
2023-03-08 17:14:39 +0100

    - Instalación moongose - Conexión db

--- database/config.js
+++ database/config.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async() => {
+class Conexion {
 
-    try {
-        await mongoose.connect("mongodb://" + process.env.DB_URL + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASE);
+    constructor() {
+        try {
+            mongoose.connect("mongodb://" + process.env.DB_URL + ":" + process.env.DB_PORT + "/" + process.env.DB_DATABASE);
+            console.log('Base de datos online');
     
-        console.log('Base de datos online');
-
-    } catch (error) {
-        console.log(error);
-        throw new Error('Error a la hora de iniciar la base de datos');
+        } catch (error) {
+            console.log(error);
+            throw new Error('Error a la hora de iniciar la base de datos');
+        }
+    
     }
 }
 
 
-module.exports = {
-    dbConnection
-}
+module.exports = Conexion;
